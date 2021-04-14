cee7793a5e3afce6879f8a60a7e94d98ba285f9c
Samuel C Moore <samuel.c.moore1221@gmail.com>
2021-04-13 19:44:51 -0500

    starting on send grid api

--- emailNotificationService.js
+++ emailNotificationService.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const sgMail = require("@sendgrid/mail")
 var nodemailer = require('nodemailer');
 var cron = require('node-cron');
 const { Octokit } = require("@octokit/core");
@@ -17,6 +18,7 @@ var transporter = nodemailer.createTransport({
   }
 });
 
+
 const sendEmail = (htmlBody, recipentsEmail, emailSubject = "GitHub Activity Reminder", emailType) => {
 
   var mailOptions = {
@@ -26,6 +28,7 @@ const sendEmail = (htmlBody, recipentsEmail, emailSubject = "GitHub Activity Rem
     html: htmlBody
   };
 
+  
   transporter.sendMail(mailOptions, function (error) {
     if (error) {
       const currentDate = DateTime.local().toString();
@@ -57,6 +60,47 @@ const didStudentPushToday = (userName) => {
     console.log(lastPushEventDate.toString());
     return dateOne.hasSame(dateTwo, 'day');
   }))
+}
+
+const checkCohortsGithubActivity = (cohortName) => {
+  const allCohorts = JSON.parse(fs.readFileSync('cohorts.json'));
+  let cohort = allCohorts.cohorts.filter(cohort => cohort.name === cohortName)[0]
+  const currentDate = DateTime.local().toLocaleString();
+
+  getAllGithubActivityStats(cohort).then(studenGithubStats => {
+    studenGithubStats.sort((a,b) => b.daysSincePush - a.daysSincePush)
+    console.log(studenGithubStats)
+    let emailbody = `<h2>Today's git activity stutus</h2>`;
+    studenGithubStats.forEach(result => emailbody += buildEmailBody(result) );
+    sendEmail(emailbody,cohort.email,`${currentDate}, ${cohort.name} Github Activity`, `${cohort.name} Github Activity Notifier`);
+  });
+
+  // cohortGithubResults.sort((a,b) => b.daysSincePush - a.daysSincePush)
+
+  
+  // console.log(cohortGithubResults)
+}
+
+const getAllGithubActivityStats = (cohort) => {
+  const { students } = cohort;
+  return Promise.all (students.map(student => {
+    const { name, github } = student;
+      return getGitHubActivityStats(github).then(result => {
+        return {...result , "name":name}
+    })
+  }))
+};
+
+
+const buildEmailBody = (githubResult) =>{
+  switch(githubResult.daysSincePush){
+    case 0:
+      return `<p>${githubResult.name} currently has no github activity for today.</p><p>${githubResult.name} last push to github was yesterday.</p><p><a href="https://github.com/${githubResult.username}">${githubResult.name}'s github</a></p><br>`;
+    case "no_activity":
+      return `<p>${githubResult.name} currently has no github activity in the past year!!.</p><a href="https://github.com/${githubResult.username}">${githubResult.name}'s github</a></p><br>`;
+    default:
+      return `<p>${githubResult.name}'s last push to github was ${githubResult.daysSincePush} days ago.</p><p><a href="https://github.com/${githubResult.username}">${githubResult.name}'s github</a></p><br>`;
+  }
 }
 
 
@@ -99,11 +143,64 @@ const checkGitHubActivity = (userName) => {
   }))
 }
 
-checkGitHubActivity("douglas-codeup");
+const getGitHubActivityStats = (userName) => {
+  return ReqStudentActivity(userName).then((responce => {
+
+    if (responce.data.length == 0) {
+
+      const currentDate = DateTime.local().toLocaleString();
+      const EmailSubject = `${currentDate}, No Github Activity`;
+      const superDisapointedEmail = `<h2>Dear ${userName}</h2><p>You currently have no github activity, this is very concerning and can negativley impact your job search.</p><p>You can view your current github activity <a href="https://github.com/${userName}">HERE</a></p>`;
+      return {"username":userName,"daysSincePush":"no_activity"};
+
+    } else if (responce.data.length > 0) {
+
+      const pushEvents = responce.data.filter(event => event.type = 'PushEvent');
+      const lastPushEventDate = DateTime.fromISO(pushEvents[0].created_at);
+      const numOfDaysSincePush = Math.floor(Math.abs(lastPushEventDate.diffNow('day').values.days));
+      const currentDate = DateTime.local().toLocaleString();
+
+      if (!lastPushEventDate.hasSame(currentDate, 'day')) {
+
+        if(numOfDaysSincePush === 0){
+          return {"username":userName,"daysSincePush":0}
+        }else{
+          return {"username":userName,"daysSincePush":numOfDaysSincePush}
+        }
+      }else console.log(`No action needed for ${userName}, activity was found for today.`);
+
+    }
+  }))
+}
+
+(async function sendEmailToAllCohorts(){
+  const allCohorts = JSON.parse(fs.readFileSync('cohorts.json'));
+  console.log(allCohorts);
+  allCohorts.cohorts.forEach((cohort,index) => {
+
+    setTimeout(() => checkCohortsGithubActivity(cohort.name), 3000 * (index + 1));
+
+    
+  });
+  // let cohort = allCohorts.cohorts.filter(cohort => cohort.name === cohortName)[0]
+
+  // allCohorts.cohorts.forEach(cohort,index => {
+  //   setTimeout(() => checkCohortsGithubActivity(cohort.name), 1000 * (index + 1));
+    
+  // });
+})()
 
 
 
-cron.schedule('50 16 * * Monday,Tuesday,Wednesday,Thursday,Friday', () => {
-  checkGitHubActivity("samuelmoorec");
-  console.log('running a task every week day at 4:50pm');
-});
+// await sendEmailToAllCohorts();
+
+// checkCohortsGithubActivity("kalypso");
+
+// checkGitHubActivity("douglas-codeup");
+
+
+
+// cron.schedule('50 16 * * Monday,Tuesday,Wednesday,Thursday,Friday', () => {
+//   checkGitHubActivity("samuelmoorec");
+//   console.log('running a task every week day at 4:50pm');
+// });
